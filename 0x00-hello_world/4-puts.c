e4830a751b12ee6ba1a2e01f73a06436233196d7
VicktrK <victorkalenda@gmail.com>
2023-02-20 10:23:45 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-
+/**
+ *main-entry point
+ *return 0- terminates program
+ */
 int main(void)
 {
-  puts(("Programming is like building a multilingual puzzle")/n);
- return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
